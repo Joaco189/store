5496b0301df86d97d9ba8ff9874789462e35edfe
Joaco189 <mangoldjoaquin@gmail.com>
2022-11-30 21:28:09 -0300

    Home component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import "./Home.css"
+import Spline from '@splinetool/react-spline';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+
+    const navigate = useNavigate()
+
     return (
-        <div>
-            <div className='home-section'></div>
+        <div className='home-container'>
+            <div className='home-content'>
+                <h2 className='welcome-text'>Bienvenido a <span>STORE</span></h2> 
+                <div className='controller-container'>
+                    <Spline scene="https://prod.spline.design/n3rH5iy3usMw6s2F/scene.splinecode" />
+                </div>
+                <div className='products-button'>
+                    <button onClick={() => navigate("/productos")}>Ver Productos</button>
+                </div>
+            </div>
         </div>
     )
 }
