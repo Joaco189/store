3a02225a67c24ec52d35bfb497a1bab6790b4327
Joaco189 <mangoldjoaquin@gmail.com>
2022-11-30 21:54:04 -0300

    adding loading

--- src/containers/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/containers/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,34 +1,43 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react'
-import { useParams } from 'react-router-dom'
-import ItemDetail from '../../components/ItemDetail/ItemDetail'
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import ItemDetail from "../../components/ItemDetail/ItemDetail";
 import { doc, getDoc } from "firebase/firestore";
-import { db } from '../../firebase/config';
-
+import { db } from "../../firebase/config";
+import "./ItemDetailContainer.css";
+import MoonLoader from "react-spinners/MoonLoader";
 
 const ItemDetailContainer = () => {
+    const { itemId } = useParams();
 
-    const {itemId} = useParams()
-
-    const [producto, setProducto] = useState({})
+    const [producto, setProducto] = useState({});
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         (async () => {
             try {
-
+                setLoading(true);
                 const docRef = doc(db, "products", itemId);
                 const docSnap = await getDoc(docRef);
-
                 if (docSnap.exists()) {
-                    setProducto({...docSnap.data(), id: docSnap.id})
-                } 
-                
+                    setProducto({ ...docSnap.data(), id: docSnap.id });
+                }
             } catch (error) {
-                console.log(error)
+                console.log(error);
+            } finally {
+                setLoading(false);
             }
-        })()
-    }, [itemId])
-    return <ItemDetail producto={producto}/>
-}
+        })();
+    }, [itemId]);
+    return (
+        <div className="item-detail-container">
+            {loading ? (
+                <MoonLoader color="#fff" size={55} speedMultiplier={0.8} />
+            ) : (
+                <ItemDetail producto={producto} />
+            )}
+        </div>
+    );
+};
 
-export default ItemDetailContainer
+export default ItemDetailContainer;
