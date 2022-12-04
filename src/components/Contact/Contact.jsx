027be43d43a60189bc0efaf907005d9b862ef267
Joaco189 <mangoldjoaquin@gmail.com>
2022-12-04 01:19:00 -0300

    adding contact component

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Contact.css"
+
+const Contact = () => {
+    return (
+        <div className='contact-container'>
+            <div className='contact-content'>
+                <h1>Nuestros numeros son:</h1>
+                <a href='tel:3547-875214'>+54 3547-875214</a>
+                <a href='tel:3547-872251'>+54 3547-872251</a>
+            </div>
+        </div>
+    )
+}
+
+export default Contact
